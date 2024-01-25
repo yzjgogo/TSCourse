c079aea75c42e00b491eed71c8a84f14387de872
yinzhijie <yzjgogo@139.com>
2024-01-25 17:56:11 +0800

    新内容添加

--- 1_hello_word.ts
+++ 1_hello_word.ts
@@ -278,17 +278,108 @@ letters.forEach(function(item,index,arr){
 
 
 /*
-   联合类型，以后会细讲
+   never类型：不可能有这样的值的类型，就是never类型，如果某个变量定义为never类型，例如：let a:never,那么就不可能有任何值赋值给a。说明不可能有某个值x可以赋值给a
+   参考：https://wangdoc.com/typescript/any#never-%E7%B1%BB%E5%9E%8B
+
+   never类型是其它任何类型的子类型，因此其它任何类型都包含了never类型，never类型是任何其他类型所共有的。因此我们可以把never类型的变量赋值给其它任意类型的变量。
+*/
+let mynev:never
+// mynev = 3 没有哪个值能赋值给never，因此报错
+
+//never类型可以赋值给其它任意类型
+// function f1724():never{//这个方法永远不会返回任何值，因此相当于返回了never类型的值，即没有值可返回。
+//    throw new Error('my error')
+// }注释掉是因为throw会报错，没法继续往下走，以后回忆的时候可以打开
+// let a1724:number = f1724()
+// let b1724:string = f1724()
+// let c1724:boolean = f1724()
+
+//比如你写某个方法，handleMessage(message: string | number){}，为了防止你离职后，别人修改这个方法新增了类型boolean，你可以在default里定义变量check:never，这样如果他只新增boolean类型而不在case "boolean"中处理这种情况，则编译阶段参数变成message: string | number | boolean后就会立刻报红
+function handleMessage(message: string | number | boolean) {
+   switch (typeof message) {
+     case "string":
+       console.log(message.length)
+       break
+     case "number":
+       console.log(message)
+       break
+       //如果注释掉这个case "boolean"下面的check就会报红，因为你会把一个boolean类型的message赋值给never类型的check，这是不对的，因为never类型不能接收任何值
+     case "boolean":
+       console.log(Number(message))
+       break
+     default:
+       const check: never = message
+   }
+ }
+ handleMessage("aaaa")
+ handleMessage(1234)
+ // 另外同事调用这个函数
+ handleMessage(true)
+
+ //联合类型需要注意的never案例
+ let lianhe: string|number
+ lianhe = "abc"//此时已经确定了linahe是string
+ if(typeof lianhe === 'number'){
+      //因为lianhe已经是string类型，所以在这个if语句里没有任何值能赋值给lianhe，因此编译器就报错
+      // lianhe.toFixed(1)
+ }
+
+
+ 
+
+
+/*
+   联合类型
 */
 let id:string|number|{key:string}
 // 赋值string类型后，可以使用string相关的方法等
 id = '123'
 id.length
+console.log("id的数据类型1：",(typeof id))//string 此时id已经是string类型
 //赋值number类型后，可以使用number相关的方法等
 id = 456
 id.toFixed(1)
+console.log("id的数据类型2：",(typeof id))//number 此时id已经是number类型
 //。。。。
 id = {key:"789"}
+console.log("id的数据类型3：",(typeof id))//object 此时id已经是object类型
+
+//使用类型缩小处理联合类型
+function handleLianhe(params: string|number|boolean){
+   if(typeof params === 'string'){
+
+   }else if(typeof params === 'number'){
+
+   }else if(typeof params === 'boolean'){
+
+   }else{
+      //params是never类型
+   }
+}
+
+
+
+/*
+   元祖数据类型tuple：
+   元祖类型有点类似数组，但是需要给每一个元素指定类型
+*/
+//1，定义元祖类型的变量
+const myinfo11:[string,number,boolean] = ['刘德华',63,false]
+console.log(myinfo11,myinfo11[0],myinfo11[1],myinfo11[2])
+
+//2，直接用元祖格式变量接收
+const [mma,mmb,mmc] = ['郭德纲',45,true]
+
+//3，函数经常返回元祖类型
+function dosomething(params:string):[number,(data:number)=>void]{
+   // console.log(params)
+   function resfun(data:number){
+      console.log("需要返回的函数",data)
+   }
+   return [parseInt(params),resfun]
+}
+const [mValue,mFunc] = dosomething('10086')
+console.log("获取返回的元祖类型:",mValue,mFunc(mValue))
 
 
 export { }
